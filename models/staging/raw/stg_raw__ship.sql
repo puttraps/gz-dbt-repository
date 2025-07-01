8dbc988f4539c4383b8dfe9be6c1457138128901
p.trapsiloningrum@gmail.com <p.trapsiloningrum@gmail.com>
2025-07-01 15:15:57 +0000

    some tests are added

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,8 +11,9 @@ renamed as (
     select
         orders_id,
         shipping_fee,
+        shipping_fee_1,
         logcost,
-        CAST (ship_cost AS FLOAT64) as ship_cost
+        ship_cost
 
     from source
 
